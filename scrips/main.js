3f4ad67844f029aef16715fc182b731617145bb9
chutebnl <braianledantes@gmail.com>
2021-01-03 18:45:28 -0300

    nuevo estilo y mas elementos

---
+++ scrips/main.js
@@ -0,0 +1,8 @@
+//alert("hola hijo de ricardo")
+var btn = document.getElementById("btn_add_ricardo")
+btn.addEventListener("click", addRicardo)
+
+function addRicardo() {
+    // TODO tiene que agregar un nuevo gif de ricardo en el div de imagenes
+    alert("agregado a ricardo")
+}
